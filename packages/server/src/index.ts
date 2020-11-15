cedd5ac1e6df32aa6eff19b320feb731dc6e9f63
Timo Glastra <timo@animo.id>
2020-11-15 01:56:36 +0100

    ...

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -107,11 +107,11 @@ async function seedDb() {
   const credType = new CredentialType();
   credType.name = 'CPR';
 
-  const emergencyType1 = new EmergencyEventType()
-  emergencyType1.code = 'fire'
-  emergencyType1.title = 'BURN BABY BABY BABY'
-  emergencyType1.credentialTypes.push(credType)
-  await emergencyTypeRepo.save(emergencyType1)
+  const emergencyType1 = new EmergencyEventType();
+  emergencyType1.code = 'fire';
+  emergencyType1.title = 'BURN BABY BABY BABY';
+  emergencyType1.credentialTypes.push(credType);
+  await emergencyTypeRepo.save(emergencyType1);
 }
 
 createConnection()
@@ -146,9 +146,10 @@ createConnection()
         }
       );
     });
-    
+
     // setup express app here
     // ...
+    await seedDb();
 
     // start express server
     server.listen(process.env.PORT);
